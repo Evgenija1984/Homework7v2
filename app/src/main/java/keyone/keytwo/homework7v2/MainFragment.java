e3440e83a11948331885f99bb3f1e46ee522be74
MEA <mayer_evgenija@mail.ru>
2021-08-20 23:47:09 +0700

    Создала MainFragment и макет fragment_main

---
+++ app/src/main/java/keyone/keytwo/homework7v2/MainFragment.java
@@ -0,0 +1,22 @@
+package keyone.keytwo.homework7v2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
