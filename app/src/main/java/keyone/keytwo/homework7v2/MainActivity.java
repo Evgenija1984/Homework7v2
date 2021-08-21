962815b9cb72ee1bbc9f83fda8fb1c7ae1a56734
MEA <mayer_evgenija@mail.ru>
2021-08-22 00:51:51 +0700

    Выбранные настройки сохраняются
    В MainActivity инициализировала кнопки, повешала на них listener

--- app/src/main/java/keyone/keytwo/homework7v2/MainActivity.java
+++ app/src/main/java/keyone/keytwo/homework7v2/MainActivity.java
@@ -1,14 +1,64 @@
 package keyone.keytwo.homework7v2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        SharedPreferences sharedPreferences = getSharedPreferences(Settings.SHARED_PREFERENCE_NAME, Context.MODE_PRIVATE);
+        Settings.isBackStack = sharedPreferences.getBoolean(Settings.IS_BACK_STACK_USED, false);
+        Settings.isAddFragment = sharedPreferences.getBoolean(Settings.IS_ADD_FRAGMENT_USED, false);
+        Settings.isReplaceFragment = sharedPreferences.getBoolean(Settings.IS_REPLACE_FRAGMENT_USED, false);
+        Settings.isBackAsRemove = sharedPreferences.getBoolean(Settings.IS_BACK_AS_REMOVE_FRAGMENT, false);
+        Settings.isDeleteBeforeAdd = sharedPreferences.getBoolean(Settings.IS_DELETE_FRAGMENT_BEFORE_ADD, false);
+    }
+
+    private void initView() {
+        Button buttonBack = findViewById(R.id.buttonBack);
+        Button buttonMain = findViewById(R.id.buttonMain);
+        Button buttonFavorite = findViewById(R.id.buttonFavorite);
+        Button buttonSettings = findViewById(R.id.buttonSettings);
+
+        buttonBack.setOnClickListener(this);
+        buttonMain.setOnClickListener(this);
+        buttonFavorite.setOnClickListener(this);
+        buttonSettings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonBack:
+
+                break;
+            case R.id.buttonMain:
+                showFragment(MainFragment.newInstance());
+                break;
+            case R.id.buttonFavorite:
+                showFragment(FavoriteFragment.newInstance());
+                break;
+            case R.id.buttonSettings:
+                showFragment(SettingsFragment.newInstance());
+                break;
+        }
+
+    }
+
+    private void showFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
