6fa65dbd6d048fac6accada13b63b7d7a3f4aa2f
MEA <mayer_evgenija@mail.ru>
2021-08-21 10:29:38 +0700

    Создала фрагменты FavoriteFragment, MainFragment; макеты для них

---
+++ app/src/main/java/keyone/keytwo/homework7v2/FavoriteFragment.java
@@ -0,0 +1,22 @@
+package keyone.keytwo.homework7v2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class FavoriteFragment extends Fragment {
+
+    public static FavoriteFragment newInstance() {
+        return new FavoriteFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        return view;
+    }
+}
